8962eeb7d2ae2cc8735991017ba6a0f68b49f168
Ying Wang <yingwan@microsoft.com>
2017-02-23 13:17:27 +0100

    bugfix, reinit ht, submission 11, 0.70543

--- combineVideoAudio.py
+++ combineVideoAudio.py
@@ -17,24 +17,25 @@ writer = csv.writer(outputfile, delimiter='|')
 readAudio = csv.reader(audiofile, delimiter=',')
 firstAudioLine = next(readAudio)
 
-
-classPro = list();
-for i in range(0, 4800):
-   classPro.append(0.0)
 #print classPro
 count = 0
+
+# for each video
 for videoRow in readVideo:
-    #print(videoRow[0],videoRow[1])
+    # read in video prediction
     labelConfidentialParisVideo = videoRow[1].split()
     # confidence of first label is high, skip audio
     if float(labelConfidentialParisVideo[1]) > 0.1:
-    	#print labelConfidentialParisVideo[1]
         skipAudioRow = next(readAudio)
         writer.writerow([videoRow[0] + ',' + videoRow[1]])
-    	#writer.writerow([videoRow[0], ',', videoRow[1]])
-    #TODO: Write video line to CSV
-    # take audio into account
+
+    # video confidence low, take audio into account
     else:
+    	# init hashtable
+    	classPro = list();
+    	for i in range(0, 4800):
+   			classPro.append(0.0)
+   		# read audio data into hashtable
         audioRow = next(readAudio)
         labelConfidentialParisAudio = audioRow[1].split()
         for i in range(0, 20):
@@ -42,25 +43,20 @@ for videoRow in readVideo:
         max = 0
         index = 0
         
-        #video confidence low, combine video and audio
+        # combine audio into video
         videoPairs = list()
         pair = namedtuple("Pair", ["label", "probablity"])
         for i in range(0, 20):
         	probablity = float(labelConfidentialParisVideo[i * 2 + 1]) * 0.615 + classPro[int(labelConfidentialParisVideo[i * 2])] * 0.385
         	videoPairs.append(pair(int(labelConfidentialParisVideo[i * 2]), probablity))
-        #print videoPairs
-        #print "----"
+        # sort the list after recalculate probablity
         videoPairs = sorted(videoPairs, key=attrgetter('probablity'), reverse=True)
-        #print videoPairs
-        #print "----------"
-        #print "----------"
         if videoPairs[0].label != labelConfidentialParisVideo[i * 2]:
         	count = count + 1
         	#print str(videoPairs[0].label) + " " + labelConfidentialParisVideo[i * 2]
         outputline = ""
         for i in range(0, 20):
         	outputline = outputline + str(videoPairs[i].label) + ' ' + str(videoPairs[i].probablity) + ' '
-        #writer = csv.writer(outputfile, delimiter ='|',quotechar =' ',quoting=csv.QUOTE_MINIMAL)
-        # writer = csv.writer(outputfile, delimiter='|')
         writer.writerow([videoRow[0] + ',' + outputline])
+# print how many entries changed 1st label
 print count
